fa87e4d0d86120612aeacf2466cac205fa1221d0
Colby Blaisdell <ccblaisdell@gmail.com>
2015-09-29 14:31:47 -0500

    Bumped version number

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule DotaApi.Mixfile do
 
   def project do
     [app: :dota_api,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
